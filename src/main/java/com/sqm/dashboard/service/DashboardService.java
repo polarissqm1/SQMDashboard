094feeb48d128aff844ce8f669ce40cb5e4dca89
anil.c <anil.c@C18002.polaris.co.in>
2015-02-25 20:56:51 +0530

    Integrated code till 25Feb21015

--- src/main/java/com/sqm/dashboard/service/DashboardService.java
+++ src/main/java/com/sqm/dashboard/service/DashboardService.java
@@ -3,5 +3,5 @@ package com.sqm.dashboard.service;
 import javax.ws.rs.core.Response;
 
 public interface DashboardService {
-	 public Response getLandingInfo(String project,String release);
+	 public Response getLandingInfo(String project,String release)throws Exception;
 }
--- src/main/java/com/sqm/dashboard/service/impl/DashboardServiceImpl.java
+++ src/main/java/com/sqm/dashboard/service/impl/DashboardServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sqm.dashboard.service.impl;
 
+import java.net.UnknownHostException;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -10,9 +11,15 @@ import javax.ws.rs.core.Response;
 
 
 
+
+
+
+
+import com.sqm.dashboard.controller.DashboardController;
 import com.sqm.dashboard.dao.DashboardDAO;
 import com.sqm.dashboard.service.DashboardService;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,12 +29,22 @@ import com.google.gson.Gson;
 
 @Service("dashboardService")
 public class DashboardServiceImpl implements DashboardService{
+	
+	
+	final Logger log=Logger.getLogger(DashboardController.class);
 
 	@Autowired
 	DashboardDAO dashboardDAO;
 	
 	@Override
-	public Response getLandingInfo(String project,String release) {
+	public Response getLandingInfo(String project,String release) throws Exception {
+		
+		try{
+			/*
+			int a[] = new int[2];
+	         System.out.println("Access element three :" + a[3]);*/
+			
+			
 		Gson gson=new Gson();
 		System.out.println(dashboardDAO.getLandingInfo(project,release).toString());
 		
@@ -36,6 +53,15 @@ public class DashboardServiceImpl implements DashboardService{
 		while(itr.hasNext()){
 			System.out.println(itr.next());
 		}
+		}
+		
+		catch (Exception e) {
+			log.debug("Service layer in project data Layer");
+			throw e;
+			
+		}
+	
+		
 		return dashboardDAO.getLandingInfo(project,release);
 	}
 
