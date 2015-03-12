e090bf2a24b02a1627ebfd6086a585198f4a7a02
. <Ajay>
2015-03-12 20:33:26 +0530

    Trend reports changes by Harish

--- src/main/java/com/sqm/dashboard/VO/DefectIdsVO.java
+++ src/main/java/com/sqm/dashboard/VO/DefectIdsVO.java
@@ -1,5 +1,42 @@
 package com.sqm.dashboard.VO;
 
 public class DefectIdsVO {
-
+	
+	private String defectId;
+	private String defectType;
+	private String defectRootCause;
+	private String defectRaisedDate;
+	private String defectFixedDate;
+	public String getDefectId() {
+		return defectId;
+	}
+	public void setDefectId(String defectId) {
+		this.defectId = defectId;
+	}
+	public String getDefectType() {
+		return defectType;
+	}
+	public void setDefectType(String defectType) {
+		this.defectType = defectType;
+	}
+	public String getDefectRootCause() {
+		return defectRootCause;
+	}
+	public void setDefectRootCause(String defectRootCause) {
+		this.defectRootCause = defectRootCause;
+	}
+	public String getDefectRaisedDate() {
+		return defectRaisedDate;
+	}
+	public void setDefectRaisedDate(String defectRaisedDate) {
+		this.defectRaisedDate = defectRaisedDate;
+	}
+	public String getDefectFixedDate() {
+		return defectFixedDate;
+	}
+	public void setDefectFixedDate(String defectFixedDate) {
+		this.defectFixedDate = defectFixedDate;
+	}
+	
+   
 }
